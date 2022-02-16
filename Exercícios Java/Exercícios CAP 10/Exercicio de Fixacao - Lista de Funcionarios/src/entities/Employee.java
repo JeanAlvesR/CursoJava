3d69f2b73501014dbd320a23f38649866a22aa0a
Jean Alves Rocha <96813984+JeanAlvesR@users.noreply.github.com>
2022-02-16 14:25:29 -0300

    Add Classe Funcionarios
    Add classe funcionarios no exe de fixação.

--- Exercícios Java/Exercícios CAP 10/Exercicio de Fixacao - Lista de Funcionarios/src/entities/Employee.java
+++ Exercícios Java/Exercícios CAP 10/Exercicio de Fixacao - Lista de Funcionarios/src/entities/Employee.java
@@ -1,4 +1,45 @@
 package entities;
 
 public class Employee {
+    private int id;
+    private String name;
+    private double salary;
+
+    public Employee(int id, String name, double salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) { //Considerei que o Id poderá ser mudado futuramente.
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void salaryIncrease (int x){
+        salary *= (1+x/100);
+    }
+    public String toString(){
+
+        return id
+                +", "
+                +name
+                +", "
+                +String.format("%.2f",salary);
+    }
 }
