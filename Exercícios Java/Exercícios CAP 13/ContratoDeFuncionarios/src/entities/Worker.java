6b8680ec55330a21df780b93bdd2802009993b1f
Jean Alves Rocha <96813984+JeanAlvesR@users.noreply.github.com>
2022-02-18 11:45:23 -0300

    Create Worker.java
    Classe Worker Criada.

---
+++ Exercícios Java/Exercícios CAP 13/ContratoDeFuncionarios/src/entities/Worker.java
@@ -0,0 +1,88 @@
+package entities;
+
+import entities.enums.WorkerLevel;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+public class Worker {
+    private String name;
+    private WorkerLevel level;
+    private Double baseSalary;
+
+    private Department department;
+    private List<HourContract> contracts = new ArrayList<>();//Lista instancia aqui.
+
+    public Worker(){}
+    //Sem lista no construtor.
+    public Worker(String name, WorkerLevel level, Double baseSalary, Department department) {
+        this.name = name;
+        this.level = level;
+        this.baseSalary = baseSalary;
+        this.department = department;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public WorkerLevel getLevel() {
+        return level;
+    }
+
+    public void setLevel(WorkerLevel level) {
+        this.level = level;
+    }
+
+    public Double getBaseSalary() {
+        return baseSalary;
+    }
+
+    public void setBaseSalary(Double baseSalary) {
+        this.baseSalary = baseSalary;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    public List<HourContract> getContracts() {
+        return contracts;
+    }
+
+    public void addContract(HourContract contract){
+        contracts.add(contract);
+    }
+
+    public void removeContract(HourContract contract){
+        contracts.remove(contract);
+    }
+
+    public double income(int year, int month){
+        double sum = baseSalary;
+        Calendar cal = Calendar.getInstance();
+        //for para procurar os contratos específicos do mês.
+        for (HourContract c:
+             contracts) {
+            //Forma de passar como parametro a data do contrato à variável cal.
+            cal.setTime(c.getDate());
+            //Forma de pegar o ano.
+            int c_year = cal.get(Calendar.YEAR);
+            int c_month = cal.get(Calendar.MONTH) + 1;//Pegar o mês. Lembrando que no Date, janeiro começa no mês 0.
+            if(year == c_year && month == c_month){
+                sum+= c.totalValue();
+            }
+        }
+        return  sum;
+    }
+}
+
