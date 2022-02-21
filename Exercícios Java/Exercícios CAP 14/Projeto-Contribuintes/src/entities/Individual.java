32381bbce5351a24f0444745bf093a2d317c487a
Jean Alves Rocha <96813984+JeanAlvesR@users.noreply.github.com>
2022-02-21 16:58:22 -0300

    Update Individual.java
    Add ternário

--- Exercícios Java/Exercícios CAP 14/Projeto-Contribuintes/src/entities/Individual.java
+++ Exercícios Java/Exercícios CAP 14/Projeto-Contribuintes/src/entities/Individual.java
@@ -22,17 +22,10 @@ public class Individual extends TaxPayer{
 
     @Override
     public Double tax() {
-        float tax = 0.0f;
-        if(annualIncome<20000.00){
-            tax = 0.15f;
+        double tax = (annualIncome < 20000.00) ? 0.15 :0.25;
+        if (healthExpenditures > 0){
+            return tax*annualIncome - (0.50*healthExpenditures);
         }
-        else{
-            tax = 0.25f;
-        }
-        if (healthExpenditures==0){
-            return tax*annualIncome;
-        }
-
-        return tax*annualIncome - (0.5*healthExpenditures);
+        return tax*annualIncome;
     }
 }
